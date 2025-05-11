17a08ea764143c8b1b430c52a6896d1d460255f2
Matěj Foukal <matfou01@gmail.com>
2025-05-11 13:27:27 +0200

    feat: drop stats

---
+++ src/4-drop_statistics.sql
@@ -0,0 +1,11 @@
+-- Delete statistics for Rental Manager tables
+
+exec dbms_stats.delete_table_stats(USER, 'OWNERS');
+exec dbms_stats.delete_table_stats(USER, 'TENANTS');
+exec dbms_stats.delete_table_stats(USER, 'FLATS');
+exec dbms_stats.delete_table_stats(USER, 'CONTRACTS');
+exec dbms_stats.delete_table_stats(USER, 'PAYMENTS');
+exec dbms_stats.delete_table_stats(USER, 'REQUESTS');
+exec dbms_stats.delete_table_stats(USER, 'EMPLOYEES');
+exec dbms_stats.delete_table_stats(USER, 'SERVICE_COMPANIES');
+exec dbms_stats.delete_table_stats(USER, 'SERVICE_ACTIONS');
